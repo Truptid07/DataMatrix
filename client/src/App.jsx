8cc672ff9b5f6664e25f5d291ebb2011b7dd4f6b
Jeet115 <jeetmistry115@gmail.com>
2025-04-28 21:33:07 +0530

    protected routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedRoute from "./ProtectedRoute";
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
