514fc1489c07876c9fb82c2e8c69bf870d455e3b
Jeet115 <jeetmistry115@gmail.com>
2025-04-30 00:57:40 +0530

    login image generated and added and logo fixed

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 const Logo = () => {
   return (
     <motion.header
-        className="p-6 bg-[#E0F7FA]"
+        className="p-6"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
