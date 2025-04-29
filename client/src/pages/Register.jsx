1e74be02cc079535657fbd5d747b3c2a072d3f71
Harmeet Singh <hsdhanjal2003@gmail.com>
2025-04-29 23:49:34 +0530

    UI/UX of Register Page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,17 @@
 import RegisterForm from "../components/RegisterForm";
+import Logo from "../components/Logo";
 
 function Register() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <RegisterForm />
-    </div>
+    <>
+      <div className="min-h-screen bg-gradient-to-br from-[#E0F7FA] to-[#B2EBF2]">
+        <Logo/>
+        <div className="flex flex-col-reverse lg:flex-row items-center justify-center gap-16 lg:gap-32 mt-20">
+          <RegisterForm/>
+        </div>
+      </div>
+
+    </>
   );
 }
 
