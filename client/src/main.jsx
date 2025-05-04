249d8b45733048218b3aea72ef14cfbeeb815fe8
Jeet115 <jeetmistry115@gmail.com>
2025-05-04 18:29:17 +0530

    upload page done with responsiveness and animations with integrations

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,15 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-
-// 🛠 Import Redux Provider and store 
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js';
+import { FileProvider } from './context/FileContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <FileProvider>
+        <App />
+      </FileProvider>
     </Provider>
   </StrictMode>
 );
