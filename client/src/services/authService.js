be17ef48d18c24d2193dc5698d834e814a1978e9
Jeet115 <jeetmistry115@gmail.com>
2025-05-04 18:32:33 +0530

    history page done with backend,frontend,intergration and animations

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-const API_URL = "http://localhost:5000/api/auth";
+
+const API_URL = `${BASE_URL}/api/auth`;
 
 export const registerUser = async (userData) => {
   const response = await axios.post(`${API_URL}/register`, userData);
